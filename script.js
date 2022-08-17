b81c49cf238b3b3d5f648a1e946e8480d1c31ff5
Dmitrii Privezentsev <dv.privezencev@mail.ru>
2022-08-17 18:23:56 +0300

    added scroll magic

--- script.js
+++ script.js
@@ -1,35 +1,76 @@
-const animItems = document.querySelectorAll('.animate');
+// init controller
+var controller = new ScrollMagic.Controller({
+	globalSceneOptions: {
+		triggerHook: 'onLeave'
+	},
+});
 
-if (animItems.length > 0) {
-	window.addEventListener('scroll', animOnScroll);
-	function animOnScroll() {
-		for (let index = 0; index < animItems.length; index++) {
-			const animItem = animItems[index];
-			const animItemHeight = animItem.offsetHeight;
-			const animItemOffset = offset(animItem).top;
-			const animStart = 4;
+// HEADER
+//scene left animationText
 
-			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+var leftTween = new TweenLite.to('.left_animation', 1, {
+	transform: 'translateX(-80vw)',
+	easing: 'easeInOutSine',
+	opacity: 0
+});
 
-			if (animItemHeight > window.innerHeight) {
-				animItemPoint = window.innerHeight - window.innerHeight / animStart;
-			}
+var leftAnimationText = new ScrollMagic.Scene({
+	triggerElement: '.triger_one',
+	duration: 100
+})
+	.setTween(leftTween)
+	.setClassToggle('.left_animation', 'active')
+	// .addIndicators({ name: '1 - add a class' }) // add indicators (requires plugin)
+	.addTo(controller);
 
-			if (
-				window.scrollY > animItemOffset - animItemPoint &&
-				window.scrollY < animItemOffset + animItemHeight
-			) {
-				animItem.classList.add('active');
-			} else {
-				animItem.classList.remove('active');
-			}
-		}
-	}
+//scene cup animation
 
-	function offset(el) {
-		const rect = el.getBoundingClientRect(),
-			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-		return { top: rect.top + scrollTop };
-	}
-	animOnScroll();
-}
+var cupTween = new TweenLite.to('.cup', 1, {
+	transform: 'translateX(50deg) translateY(50deg)',
+	rotate: '240deg',
+	delay: 1
+});
+
+var cupAnimation = new ScrollMagic.Scene({
+	triggerElement: '.triger_one',
+	duration: 800,
+})
+	.setTween(cupTween)
+	.setClassToggle('.cup', 'active')
+	.addTo(controller);
+
+//scene sandwich animation
+
+var sandwichTwin = new TweenLite.to('.sandwich', 2, {
+	transform: 'translateX(20vw) translateY(20vw) scale(20)',
+	easing: 'easeInOutSine',
+	delay: 2,
+	rotate: '240deg'
+});
+
+var sandwichAnimation = new ScrollMagic.Scene({
+	triggerElement: '.triger_one',
+	duration: 600
+})
+	.setTween(sandwichTwin)
+	.setClassToggle('.sandwich', 'active')
+	// .addIndicators({ name: '2 - add a class' }) // add indicators (requires plugin)
+	.addTo(controller);
+
+//scene right animationText
+
+var rightTween = new TweenLite.to('.right_animation', 1, {
+	transform: 'translateX(80vw)',
+	easing: 'easeInOutSine',
+	opacity: 0
+});
+
+var rightAnimationText = new ScrollMagic.Scene({
+	triggerElement: '.triger_one',
+	duration: 100
+})
+	.setTween(rightTween)
+	.setClassToggle('.left_animation', 'active')
+	.addTo(controller);
+
+// CONTENT
