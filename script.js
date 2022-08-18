9bc08f5009442d0b91686880495b77ec2a9751ee
Dmitrii Privezentsev <dv.privezencev@mail.ru>
2022-08-18 15:57:40 +0300

    fix animation header

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const leftAnimationText = new ScrollMagic.Scene({
 })
 	.setTween(leftTween)
 	.setClassToggle('.left_animation', 'active')
-	.addIndicators({ name: 'text - add a class' }) // add indicators (requires plugin)
+	// .addIndicators({ name: 'text - add a class' }) // add indicators (requires plugin)
 	.addTo(controller);
 
 //scene cup animation
@@ -27,27 +27,25 @@ const cupTween = new TweenLite.to('.cup', {
 
 const cupAnimation = new ScrollMagic.Scene({
 	triggerElement: '.triger_one',
-	offset: 450,
+	offset: 300,
 })
 	.setTween(cupTween)
 	.setClassToggle('.cup', 'active')
-	.addIndicators({ name: 'cup - add a class' }) // add indicators (requires plugin)
+	// .addIndicators({ name: 'cup - add a class' }) // add indicators (requires plugin)
 	.addTo(controller);
 
 //scene sandwich animation
 
-const sandwichTwin = new TweenLite.to('.sandwich', {
-	delay: 4,
-});
+const sandwichTwin = new TweenLite.to('.sandwich', 2);
 
 const sandwichAnimation = new ScrollMagic.Scene({
 	triggerElement: '.triger_one',
-	duration: '115%',
-	offset: 800,
+	duration: '140%',
+	offset: 500,
 })
 	.setTween(sandwichTwin)
 	.setClassToggle('.sandwich', 'active')
-	.addIndicators({ name: 'sandwich - add a class' }) // add indicators (requires plugin)
+	// .addIndicators({ name: 'sandwich - add a class' }) // add indicators (requires plugin)
 	.addTo(controller);
 
 //scene right animationText
