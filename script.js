ef83d2d79412c0ac209505741d3ed1d4c95015db
Dmitrii Privezentsev <dv.privezencev@mail.ru>
2022-08-30 18:08:05 +0300

    test

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.onload = function () {
 	window.setTimeout(function () {
 		document.body.classList.add('loaded');
 		document.body.classList.remove('loaded');
-	}, 5000);
+	}, 300);
 };
 
 gsap.registerPlugin(ScrollTrigger);
